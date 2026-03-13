98451fa0ae7526fcffda7b2c4c48b8481afff23e
Venkatesh Prasad <venkatesh.dprasad@juspay.in>
2026-03-13 17:07:10 +0530

    Offer screen changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom'
 import AIChatWidget from './components/AIChatWidget'
+import OffersScreen from './screens/OffersScreen'
 import './App.css'
 
 function App() {
   return (
-    <div className="app-container">
-      <AIChatWidget />
-    </div>
+    <Router>
+      <div className="app-container">
+        <nav className="app-nav">
+          <NavLink to="/" className="nav-link" end>Chat</NavLink>
+          <NavLink to="/offers" className="nav-link">Offers</NavLink>
+        </nav>
+        <Routes>
+          <Route path="/" element={<AIChatWidget />} />
+          <Route path="/offers" element={<OffersScreen />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
--- src/components/AIChatWidget.jsx
+++ src/components/AIChatWidget.jsx
@@ -226,8 +226,7 @@ function AIChatWidget() {
                 <path d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09z" />
               </svg>
             </div>
-            <h2>Hello! I'm your AI Assistant.</h2>
-            <p>Choose an input method below to get started.</p>
+
           </div>
         ) : (
           <div className="ai-messages-list">
